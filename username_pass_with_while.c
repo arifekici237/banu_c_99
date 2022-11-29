f765da619184e648ec1e19145970dde0e3bafbe9
arifekici237 <119411659+arifekici237@users.noreply.github.com>
2022-11-29 14:58:10 +0300

    Update username_pass_with_while.c

--- username_pass_with_while.c
+++ username_pass_with_while.c
@@ -1,1 +1,31 @@
-// cretae a username_pass_with_while. 
+#include <stdio.h>
+#include <string.h>
+#define user "arif"
+#define pass "123Pass!"        
+int main()
+{
+    char username[20], password[20];
+
+    //-----LOGIN PHASE-----
+    printf("Please Enter username:");
+    scanf("%s", &username);
+
+    printf("\nPlease enter password:");
+    scanf("%s", &password);
+    
+    
+
+        printf("Wrong username or password try again!\n");
+
+        printf("\nPlease Enter username:");
+        scanf("%s", &username);
+
+        printf("\nPlease enter password:");
+        scanf("%s", &password);
+
+    
+    while (strcmp(username, user) != 0 && strcmp(password, pass) != 0);
+
+    printf("Correct username and password.\n\nWelcome Mrs.arif!\n\nWhat would you like to do?");
+    //-----END OF LOGIN PHASE-----
+    return 0;}
